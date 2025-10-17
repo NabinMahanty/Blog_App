df099dd4e71a9afb2a94a42871914e75f68ee34e
NabinMahanty <nabinmahanty2003@gmail.com>
2025-10-17 10:43:56 +0530

    optimizing folder structure

--- backend/config/kyes.js
+++ backend/config/kyes.js
@@ -2,7 +2,8 @@ const { PORT, CONNECTION_URL, JWT_SECT, EMAIL_USER, EMAIL_PASS } = process.env;
 
 module.exports = {
   port: PORT,
-  connectionurl: CONNECTION_URL,
+  connectionurl:
+    "mongodb+srv://nabinmahanty_db_user:Q%40werty00@cluster0.pe04ik3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   jwtsecret: JWT_SECT,
   email: EMAIL_USER,
   pass: EMAIL_PASS,
